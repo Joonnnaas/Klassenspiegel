1ab4a2e75bffca0b895ee0e8b7d53583e98deb39
Dorfinger Jonas <it170133@EDU.HTL-LEONDING.AC.AT>
2018-11-14 13:34:47 +0100

    Added the concept

---
+++ script/data.js
@@ -0,0 +1,34 @@
+//Klassen
+
+
+klassen['1ahitm']['schueler'] = new Array();
+['schueler'] = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10'
+]
+
+klassen['2ahitm']['schueler'] = new Array();
+['schueler'] = [
+  '1a',
+  '2a',
+  '3a',
+  '4a',
+  '5a',
+  '6a',
+  '7a',
+  '8a',
+  '9a',
+  '10a'
+]
+
+function writeKlassen () {
+
+}
---
+++ script/intitialize.js
@@ -0,0 +1,18 @@
+const classes = 4;
+
+function getDropDown() {
+
+  let klassen = new Array(4)
+  klassen['1ahitm'] = new Array();
+  klassen['1bhitm'] = new Array();
+  klassen['2ahitm'] = new Array();
+  klassen['2bhitm'] = new Array();
+  klassen['3ahitm'] = new Array();
+  klassen['3bhitm'] = new Array();
+  klassen['4ahitm'] = new Array();
+  klassen['4bhitm'] = new Array();
+
+  for (var i in klassen) {
+    alert(i);
+  }
+}
