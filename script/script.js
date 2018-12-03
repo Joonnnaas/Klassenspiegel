a1ef8e490a44072f00bbfd4cde5feaac4c1cb064
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2018-12-03 20:20:50 +0100

    added getClass() to display the specific classes

--- script/script.js
+++ script/script.js
@@ -16,3 +16,42 @@ function getInput() {
   i++;
 
 }
+
+function getClass() {
+  let klassen = document.getElementById('klasse').value;
+
+  if (klassen === '1ahitm') {
+    display1a();
+  } else if (klassen === '1bhitm') {
+    display1b();
+  } else if (klassen === '2bhitm') {
+    display2b();
+  } else if (klassen === '2ahitm') {
+    display2a();
+  }
+}
+
+function display1a() {
+  let a1 = document.getElementById('schueler1a').style.display = 'block';
+  let a2 = document.getElementById('schueler2a').style.display = 'none';
+  let b1 = document.getElementById('schueler1b').style.display = 'none';
+  let b2 = document.getElementById('schueler2b').style.display = 'none';
+}
+function display2a() {
+  let a1 = document.getElementById('schueler1a').style.display = 'none';
+  let a2 = document.getElementById('schueler2a').style.display = 'block';
+  let b1 = document.getElementById('schueler1b').style.display = 'none';
+  let b2 = document.getElementById('schueler2b').style.display = 'none';
+}
+function display1b() {
+  let a1 = document.getElementById('schueler1a').style.display = 'none';
+  let a2 = document.getElementById('schueler2a').style.display = 'none';
+  let b1 = document.getElementById('schueler1b').style.display = 'block';
+  let b2 = document.getElementById('schueler2b').style.display = 'none';
+}
+function display2b() {
+  let a1 = document.getElementById('schueler1a').style.display = 'none';
+  let a2 = document.getElementById('schueler2a').style.display = 'none';
+  let b1 = document.getElementById('schueler1b').style.display = 'none';
+  let b2 = document.getElementById('schueler2b').style.display = 'block';
+}
