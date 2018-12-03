83802ff238dca277c11fe9b4899f81c8ebb7b641
Joonnnaas <36422131+Joonnnaas@users.noreply.github.com>
2018-12-03 20:41:09 +0100

    Added getStudent()

--- script/script.js
+++ script/script.js
@@ -1,20 +1,15 @@
 let i = 1;
 
 function getInput() {
-  let name = document.getElementById('name').value;
+  let name = getStudentName();
   let klasse = document.getElementById('klasse').value;
   let subject = document.getElementById('subject').value;
   let mark = document.getElementById('mark').value;
 
-  document.getElementById('td' + i).innerHTML = name;
-  i++;
-  document.getElementById('td' + i).innerHTML = klasse;
-  i++;
-  document.getElementById('td' + i).innerHTML = subject;
-  i++;
-  document.getElementById('td' + i).innerHTML = mark;
-  i++;
-
+  alert(klasse);
+  alert(name);
+  alert(subject);
+  alert(mark);
 }
 
 function getClass() {
@@ -37,21 +32,40 @@ function display1a() {
   let b1 = document.getElementById('schueler1b').style.display = 'none';
   let b2 = document.getElementById('schueler2b').style.display = 'none';
 }
+
 function display2a() {
   let a1 = document.getElementById('schueler1a').style.display = 'none';
   let a2 = document.getElementById('schueler2a').style.display = 'block';
   let b1 = document.getElementById('schueler1b').style.display = 'none';
   let b2 = document.getElementById('schueler2b').style.display = 'none';
 }
+
 function display1b() {
   let a1 = document.getElementById('schueler1a').style.display = 'none';
   let a2 = document.getElementById('schueler2a').style.display = 'none';
   let b1 = document.getElementById('schueler1b').style.display = 'block';
   let b2 = document.getElementById('schueler2b').style.display = 'none';
 }
+
 function display2b() {
   let a1 = document.getElementById('schueler1a').style.display = 'none';
   let a2 = document.getElementById('schueler2a').style.display = 'none';
   let b1 = document.getElementById('schueler1b').style.display = 'none';
   let b2 = document.getElementById('schueler2b').style.display = 'block';
+}
+
+function getStudentName() {
+  let name;
+  let klassen = document.getElementById('klasse').value;
+
+  if (klassen === '1ahitm') {
+    name = document.getElementById('schueler1a').value;
+  } else if (klassen === '1bhitm') {
+    name = document.getElementById('schueler1b').value;
+  } else if (klassen === '2bhitm') {
+    name = document.getElementById('schueler2b').value;
+  } else if (klassen === '2ahitm') {
+    name = document.getElementById('schueler2a').value;
+  }
+  return name;
 }
